ea3a1a2c706f3f4ccdc6d8bb8f3ae14887618b8a
unknown <kirill@kirill-.(none)>
2012-11-02 19:29:50 +0400

    111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111

--- ITMOPrelude/Categories/MonadFish.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-module ITMOPrelude.Categories.MonadFish where
-
-class MonadFish m where
-  returnFish :: a -> m a
-  (>=>) :: (a -> m b) -> (b -> m c) -> a -> m c
--- ITMOPrelude/Categories/MonadJoin.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-module ITMOPrelude.Categories.MonadJoin where
-
-import ITMOPrelude.Categories
-
-class Functor m => MonadJoin m where
-  returnJoin :: a -> m a
-  join :: m (m a) -> m a
